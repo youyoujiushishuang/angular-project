aa63024a5ced72345019b0117533eb109b4b2614
yj-shuang <497706870@qq.com>
2019-03-14 14:28:07 +0800

    响应式注册表单--数据模型与页面表单绑定结束

--- src/app/register-form/register-form.component.ts
+++ src/app/register-form/register-form.component.ts
@@ -8,10 +8,10 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class RegisterFormComponent implements OnInit {
   //1.声明一个formGroup 是整个数据结构
-  formGroup:FormGroup
+  formModel:FormGroup
   constructor() {
     //在构造函数中声明formGroup 这个属性,是一个对象,里面有用户名,手机号两个表单 formControl , 还有一个formGroup 里面是密码和确认密码
-    this.formGroup = new FormGroup({
+    this.formModel = new FormGroup({
       username:new FormControl(),
       mobile:new FormControl(),
       passwordsGroup:new FormGroup({
@@ -24,7 +24,7 @@ export class RegisterFormComponent implements OnInit {
   ngOnInit() {
   }
   OnSubmit(){
-    console.log(this.formGroup.value)
+    console.log(this.formModel.value)
   }
 
 }
