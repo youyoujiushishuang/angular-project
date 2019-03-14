eb562e194f15596582da46057dbf87da3ea10a08
yj-shuang <497706870@qq.com>
2019-03-14 14:05:32 +0800

    响应式表单的指令--formControl

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -9,12 +9,13 @@ import { FormControl, FormGroup, FormArray } from '@angular/forms';
 export class ReactiveFormComponent implements OnInit {
 
   //如果将这个username与页面上的表单联系起来的话,那么这个表单的初始值就是aaa
-  username:FormControl = new FormControl("aaa")
+  // username:FormControl = new FormControl("aaa")
 
   //创建一个FormGroup
   formModel:FormGroup = new FormGroup({
     //里面包含一个起始日期和一个终止日期
     //声明一个FormGroup
+    username:new FormControl("aaa"),
     dateRange:new FormGroup({
       from: new FormControl(),
       to: new FormControl()
