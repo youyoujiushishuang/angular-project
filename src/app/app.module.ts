a1c084fcf7a8a18a2e4d88245c0acfdba79e6b35
yj-shuang <497706870@qq.com>
2019-03-14 11:19:51 +0800

    模板式表单---ngForm  ngModel  模板本地变量

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TemplateFormComponent } from './template-form/template-form.component';
+import { FormsModule ,ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/template-form/template-form.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-template-form',
+  templateUrl: './template-form.component.html',
+  styleUrls: ['./template-form.component.less']
+})
+export class TemplateFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  OnSubmit(value:any){
+    console.log(value)
+  }
+}
